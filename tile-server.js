eb17423f12f8d322d0ce686d4d1df8c877dc0521
Victor Parmar <victorparmar@gmail.com>
2015-06-25 00:05:09 +0200

    prettify

--- tile-server.js
+++ tile-server.js
@@ -4,7 +4,6 @@
 // global modules
 //
 
-
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
@@ -48,7 +47,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 if ('development' == env) {
-	app.use(errorHandler());
+  app.use(errorHandler());
 }
 
 //
@@ -73,30 +72,30 @@ api.use(cors.allowAll);
 
 var parseResult = function (result) {
 
-	return new Promise(function (resolve) {
+  return new Promise(function (resolve) {
 
-		var features = [];
-		var featureCollection = {};
-		featureCollection.type = 'FeatureCollection';
+    var features = [];
+    var featureCollection = {};
+    featureCollection.type = 'FeatureCollection';
 
-		for (var r = 0 ; r < result.length ; r++) {
+    for (var r = 0 ; r < result.length ; r++) {
 
-			var feature = {};
-			feature.type = 'Feature';
+      var feature = {};
+      feature.type = 'Feature';
 
 
-			if (result[r].geometry) {
-				feature.geometry = JSON.parse(result[r].geometry);
-			}
+      if (result[r].geometry) {
+        feature.geometry = JSON.parse(result[r].geometry);
+      }
 
-			features.push(feature);
-		}
+      features.push(feature);
+    }
 
-		featureCollection.features = features;
+    featureCollection.features = features;
 
-		resolve(featureCollection);
+    resolve(featureCollection);
 
-	});
+  });
 }
 
 //
@@ -106,103 +105,103 @@ var parseResult = function (result) {
 function init()
 {
 
-	//
-	// app routes
-	//
+  //
+  // app routes
+  //
 
-	app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));
 
-	//
-	// api routes
-	//
+  //
+  // api routes
+  //
 
-	api.get('/', function (req, res, next) {
-		res.send('yo');
-	});
+  api.get('/', function (req, res, next) {
+    res.send('yo');
+  });
 
-	api.get('/stats/cache', function (req, res, next) {
+  api.get('/stats/cache', function (req, res, next) {
 
-		res.send(cache.getStats());
+    res.send(cache.getStats());
 
-	});
+  });
 
-	api.get('/tiles/vector/:z/:x/:y', function (req, res, next) {
+  api.get('/tiles/vector/:z/:x/:y', function (req, res, next) {
 
-		try {
+    try {
 
-			var z = parseInt(req.params.z);
-			var x = parseInt(req.params.x);
-			var y = parseInt(req.params.y);
+      var z = parseInt(req.params.z);
+      var x = parseInt(req.params.x);
+      var y = parseInt(req.params.y);
 
-			var key = z + '-' + x + '-' + y;
+      var key = z + '-' + x + '-' + y;
 
-			var c = cache.get(key);
+      var c = cache.get(key);
 
-			if (c) {
-				res.send(c);
-				return;
-			}
+      if (c) {
+        res.send(c);
+        return;
+      }
 
-			var box = util.getLTRBbox(z, x, y);
+      var box = util.getLTRBbox(z, x, y);
 
-			db.getTileData(box.left, box.top, box.right, box.bottom)
-				.then(function (data) {
+      db.getTileData(box.left, box.top, box.right, box.bottom)
+        .then(function (data) {
 
-					return parseResult(data);
+          return parseResult(data);
 
-				})
-				.then(function (featureCollection) {
+        })
+        .then(function (featureCollection) {
 
-					cache.set(key, featureCollection);
-					res.send(featureCollection);
+          cache.set(key, featureCollection);
+          res.send(featureCollection);
 
-				})
-				.catch(function (err) {
-					logger.error(err);
-					next(err);
-				});
-
-
-		} catch (err) {
-
-			logger.error(err);
-			next(err);
-		}
-
-	});
-
-	//
-	// error handling
-	//
-
-	app.use(function (err, req, res, next) {
-
-		logger.error(err);
-		res.header('Error', err);
-		res.status(500).send();
-
-	});
-
-	api.use(function (err, req, res, next) {
-
-		logger.error(err);
-		res.header('Error', err);
-		res.status(500).send();
-
-	});
-
-	//
-	// setup
-	//
-
-	var app_http = app.listen(app_port);
-	logger.info('app listening on ' + app_port);
-
-	var api_http = api.listen(api_port);
-	logger.info('api listening on ' + api_port);
+        })
+        .catch(function (err) {
+          logger.error(err);
+          next(err);
+        });
 
 
-	logger.info('running in ' + env);
+    } catch (err) {
+
+      logger.error(err);
+      next(err);
+    }
+
+  });
+
+  //
+  // error handling
+  //
+
+  app.use(function (err, req, res, next) {
+
+    logger.error(err);
+    res.header('Error', err);
+    res.status(500).send();
+
+  });
+
+  api.use(function (err, req, res, next) {
+
+    logger.error(err);
+    res.header('Error', err);
+    res.status(500).send();
+
+  });
+
+  //
+  // setup
+  //
+
+  var app_http = app.listen(app_port);
+  logger.info('app listening on ' + app_port);
+
+  var api_http = api.listen(api_port);
+  logger.info('api listening on ' + api_port);
+
+
+  logger.info('running in ' + env);
 }
 
 //
@@ -218,5 +217,5 @@ exports.init = init;
 
 if (!module.parent)
 {
-	init();
+  init();
 };
